40b3b477a1c4d9d585036a8648391ce4f76e1af0
Qaiser Abbas <mqaiser617@gmail.com>
2023-01-19 17:45:40 +0500

    Update bees_openvino_fastapi.py

--- bees_openvino_fastapi.py
+++ bees_openvino_fastapi.py
@@ -22,15 +22,7 @@ def read_file_as_image(data) -> np.ndarray:
 async def predict(file: UploadFile = File(...)):
     image = read_file_as_image(await file.read())
     img_batch = np.expand_dims(image, 0)
-
     result_infer = compiled_model([img_batch])[output_layer]
     result_index = np.argmax(result_infer[0])
-    
-    # index = np.argmax(predictions[0])
     class_name = CLASSES[result_index]
-
-    # predicted_class = class_names[np.argmax(predictions[0])]
-    # confidence = np.max(result_index[0])
-    # confidence = round(confidence, 4)
     return {"class": class_name}
-    # return {"class": class_name, "confidence": float(round(confidence, 4))}
